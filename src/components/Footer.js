5e24e70a7b8568a8939e11a4acd15dedc6f193ce
Siddhartha <siddpurwar@gmail.com>
2022-05-19 01:43:55 +0530

    Refactor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
     return (
         <div className="footer">
-            <img src="./linkedin_icon.png" />
-            <img src="./git_icon.png" />
-            <img src="./linkedin_icon.png" />
-            <img src="./git_icon.png" />
+            <a href="mailto:siddpurwar@gmail.com"><img src="./email_icon.png" /></a>
+            <a href="https://github.com/sidd6p/"><img src="./git_icon.png" /></a>
+            <a href="https://www.linkedin.com/in/siddhartha-purwar/"><img src="./linkedin_icon.png" /></a>
+            <a href="https://leetcode.com/siddp6/"><img src="./leetcode_icon.png" /></a>
         </div>
     )
 }
