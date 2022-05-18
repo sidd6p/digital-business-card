b13a907cd6b3af090875ff52c55610ae16e03b66
Siddhartha <siddpurwar@gmail.com>
2022-05-19 00:52:01 +0530

    Interests Done

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,5 +1,12 @@
 export default function Interests() {
     return (
-        <h1>Interest</h1>
+        <div className="interests">
+            <div className="heading">
+                Interest
+            </div>
+            <div className="detail">
+                I am an aspiring full-stack developer and have interest in DevOps dimensions; and also love to play with data. I work with python mostly and also use HTML/CSS/JavaScript. Leaning Azure is also a part of my day. I have a basic understanding of data analysis, SQL and marketing. Apart from this, I do competitive programming as a hobby Some of the transferable skills that I have are design thinking, solving problems, critical thinking, and Leadership.
+            </div>
+        </div>
     )
 }
